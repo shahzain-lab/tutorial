e6ffd8fc83b9c688807ba2e10e42437820530542
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-06 15:48:52 -0800

    creating relationship

--- src/server.js
+++ src/server.js
@@ -1,14 +1,22 @@
-import { createServer, Model} from "miragejs"
+import { createServer, Model, hasMany, belongsTo} from "miragejs"
 
 export default function () {
   return createServer({
     models:{
-        reminder: Model,
+        list: Model.extend({
+           reminder: hasMany() 
+        }),
+        reminder: Model.extend({
+            list: belongsTo()
+        }),
     },
     seeds(server) {
         server.create("reminder", { text: "Walk the dog" })
         server.create("reminder", { text: "Take out the trash" })
         server.create("reminder", { text: "Work out" })
+
+        server.create('list', {name: 'zain'})
+        server.create('list', {name: 'zainZ'})
       },
     routes() {
       this.get("/api/reminders", (schema) => {
@@ -22,6 +30,10 @@ export default function () {
      this.delete('api/reminders/:id',(schema,request) => {
         let id = request.params.id 
         return schema.reminders.find(id).destroy()
+     })
+     ////Lists
+     this.get('/api/lists',(schema,request) => {
+         return schema.lists.all()
      })
     },
   })
