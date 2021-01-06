bc383f3e43b19be7cc66e65639564306274e808e
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-06 15:28:10 -0800

    created seeds in server.js

--- src/server.js
+++ src/server.js
@@ -5,7 +5,11 @@ export default function () {
     models:{
         reminder: Model,
     },
-    
+    seeds(server) {
+        server.create("reminder", { text: "Walk the dog" })
+        server.create("reminder", { text: "Take out the trash" })
+        server.create("reminder", { text: "Work out" })
+      },
     routes() {
       this.get("/api/reminders", (schema) => {
           return schema.reminders.all()
